98014a4bc7c4adb325ef8a293c7946065de46a3d
Louis Collet <colletlouis@gmail.com>
2020-04-09 14:52:28 +0200

    Added state

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import Contact from './components/Contacts';
 import './components/style.css';
+import users from './components/ContactList'
 
 function App() {
   return (
     <div className="App">
-      <Contact />
+      <Contact {...users[0]}/>
+      <Contact {...users[1]}/>
+      <Contact {...users[2]}/>
+      <Contact {...users[3]}/>
+      <Contact {...users[4]}/>
     </div>
   );
 }
--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -1,22 +1,49 @@
 import React from 'react';
-import users from './ContactList'
 
-const Contact = () => (
-    <ul>
-        {users.map( user => (
-            <div key={user.name} className="Contact">
-                <img src={user.avatar} alt="Contact" className="avatar"/>
+// const Contact = () => (
+//     <ul>
+//         {users.map( user => (
+//             <div key={user.name} className="Contact">
+//                 <img src={user.avatar} alt="Contact" className="avatar"/>
+//                 <div>
+//                     <h3 className="name">{user.name}</h3>
+//                     <div className="status">
+//                         <div className={user.online ? 'status-online' : 'status-offline'}></div>
+//                         <p className='status-text'>{user.online ? 'Online' : 'Offline'}</p>
+//                     </div>
+//                 </div>
+//             </div>
+//         ))}
+//     </ul>
+// );
+
+class Contacts extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            online: props.online,
+          };
+    }
+    render() {
+        return (
+        <ul>
+            <div key={this.props.name} className="Contact">
+                <img src={this.props.avatar} alt="Contact" className="avatar"/>
                 <div>
-                    <h3 className="name">{user.name}</h3>
-                    <div className="status">
-                        <div className={user.online ? 'status-online' : 'status-offline'}></div>
-                        <p className='status-text'>{user.online ? 'Online' : 'Offline'}</p>
+                    <h3 className="name">{this.props.name}</h3>
+                    <div className="status" onClick={event => {
+                        const newOnline = !this.state.online;
+                        this.setState({online : newOnline})
+                    }}>
+                        <div className={this.state.online ? 'status-online' : 'status-offline'}></div>
+                        <p className='status-text'>{this.state.online ? 'Online' : 'Offline'}</p>
                     </div>
                 </div>
             </div>
-        ))}
-    </ul>
-);
+        </ul>
+        );
+    }
+}
 
 
-export default Contact;
+export default Contacts;
